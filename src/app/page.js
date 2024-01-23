67b30ea6e73aaf33348dc377f93101180673e97b
Sam Zafar <szafar@geniusfixers.com>
2024-01-24 01:00:13 +0500

    sam

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import "./globals.css";
 const page = () => {
   return (
     <>
+    <title>Sam Zafar Front-End Developer</title>
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
